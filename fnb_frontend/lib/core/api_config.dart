2ce8be34ed59257dff684cc2a6f8b237d930c6bf
Raditya Tresna Dwimurti <raditiya.tresna.dwimurti18@gmail.com>
2026-08-15 19:19:33 +0700

    fix blokir http

--- fnb_frontend/lib/core/api_config.dart
+++ fnb_frontend/lib/core/api_config.dart
@@ -3,7 +3,8 @@ class ApiConfig {
   // static const String baseUrl = 'http://127.0.0.1:8000/api';
 
   //INI ADALAH API UTAMA
-  static const String baseUrl = 'http://104.197.222.12/api';
+  static const String baseUrl =
+      'https://potato-das-random-annual.trycloudflare.com/api';
 
   static const String menus = '$baseUrl/menus';
   static const String promos = '$baseUrl/promos';
